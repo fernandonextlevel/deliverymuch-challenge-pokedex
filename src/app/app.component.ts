416bf4f25b3540f2133b3ae8583d9606b76dac81
Fernando <fernando.nextlevel@gmail.com>
2020-12-16 18:38:27 -0300

    Coverage 54.79%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 //Angular
 import { Component } from '@angular/core'
 
-//Services
-import { TemplateService } from './_shared/services/template-service/template-service'
-
 @Component({
 	selector: 'pokedex-root',
 	templateUrl: './app.component.html',
@@ -11,21 +8,9 @@ import { TemplateService } from './_shared/services/template-service/template-se
 })
 
 export class AppComponent {
-
-	//Props
-	public newThemeColor: string = '#dddddd'
-  
 	constructor (
-		private templateService: TemplateService
+
 	) {
 
   	}
-
-	ngOnInit(): void {
-		this.setTheme()
-	}
-  
-	setTheme(): void {
-		this.templateService.calculateColors(this.newThemeColor)
-	}
 }
